9e3c848647b10931c211a129563828787812e7c0
shanME <giorgie103188@gmail.com>
2024-07-22 20:57:05 +0400

    transaction component

--- expensetracker/src/components/TransactionList.js
+++ expensetracker/src/components/TransactionList.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const TransactionList = () => {
+  const context = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {context.transactions.map((transaction) => (
+          <li key={transaction.id} className="minus">
+            {transaction.text} <span>-$400</span>
+            <button className="delete-btn">X</button>
+          </li>
+        ))}
       </ul>
     </>
   );
